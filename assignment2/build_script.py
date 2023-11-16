594b3a68be14d5f15728f71a8c0fd9d8d8d016bb
dev <koonshen.goh@gmail.com>
2023-11-16 12:42:56 +0800

    Initial implementation of build script

--- assignment2/build_script.py
+++ assignment2/build_script.py
@@ -1,22 +1,42 @@
+# Python 3.10
 import os
 import re
+import stat
+import pathlib
 
-def updateSconstruct():
+def updateSconstruct(SConstructPath: pathlib.Path, BuildNum: str):
     # "Update the build number in the SConstruct file"
-    os.chmod(os.path.join(os.environ["SourcePath"],"develop","global","src","SConstruct"), 0755)
-    fin = open(os.path.join(os.environ["SourcePath"],"develop","global","src","SConstruct"), 'r')
-    fout = open(os.path.join(os.environ["SourcePath"],"develop","global","src","SConstruct1"), 'w')
-    for line in fin:
-        line=re.sub("point\=[\d]+","point="+os.environ["BuildNum"],line)
-        fout.write(line)
-    fin.close()
-    fout.close()
-    os.remove(os.path.join(os.environ["SourcePath"],"develop","global","src","SConstruct"))
-    os.rename(os.path.join(os.environ["SourcePath"],"develop","global","src","SConstruct1"),
-    os.path.join(os.environ["SourcePath"],"develop","global","src","SConstruct"))
+    # Assumes file size is small, read entire file into memory
+    buffer = open(SConstructPath, 'r').read()
+    # Replace build number
+    regexPattern = "(config\.version\s*=\s*Version\(.*point\s*=\s*)(\d+)(.*\))"
+    regexSubsitution = f'\g<1>{BuildNum}\g<3>'
+    buffer = re.sub(regexPattern, regexSubsitution, buffer, 1, flags=re.DOTALL)
+    # Overwrite file
+    with open(SConstructPath, 'w') as fout:
+        fout.write(buffer)
 
-def updateVersion():
+def updateVersion(VersionPath: pathlib.Path, BuildNum: str):
     # "Update the build number in the VERSION file"
-    os.chmod(os.path.join(os.environ["SourcePath"],"develop","global","src","VERSION"), 0755)
-    fin = open(os.path.join(os.environ["SourcePath"],"develop","global","src","VERSION"), 'r')
+    # Assumes file size is small, read entire file into memory
+    buffer = open(VersionPath, 'r').read()
+    # Replace build number
+    regexPattern = "(ADLMSDK_VERSION_POINT\s*=\s*)(\d+)"
+    regexSubsitution = f'\g<1>{BuildNum}'
+    buffer = re.sub(regexPattern, regexSubsitution, buffer, 1, flags=re.DOTALL)
+    # Overwrite file
+    with open(VersionPath, 'w') as fout:
+        fout.write(buffer)
 
+if __name__ == "__main__":
+    SourcePath = os.environ["SourcePath"]
+    BuildNum = os.environ["BuildNum"]
+    
+    SConstructPath = pathlib.Path(SourcePath, "develop", "global", "src", "SConstruct")
+    VersionPath = pathlib.Path(SourcePath, "develop", "global", "src", "VERSION")
+    # mode = 0755, rwx r-x r-x
+    fileMode = stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH
+    os.chmod(SConstructPath, fileMode)
+    updateSconstruct(SConstructPath, BuildNum)
+    os.chmod(VersionPath, fileMode)
+    updateVersion(VersionPath, BuildNum)
