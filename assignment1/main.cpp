453a463b240d3a6328ac9ab4cf2d7cb1aa2063ae
dev <koonshen.goh@gmail.com>
2023-11-16 09:29:03 +0800

    Added suggested template for assignment 1

---
+++ assignment1/main.cpp
@@ -0,0 +1,16 @@
+// Compiled with g++, c++17
+#include <string>
+#include <cassert>
+
+std::string reverse_words(const std::string &str)
+{
+    // TODO: Implement this function
+    return "???";
+}
+
+int main()
+{
+    std::string test_str = "String; 2be reversed...";
+    assert(reverse_words(test_str) == "gnirtS; eb2 desrever...");
+    return 0;
+}
