cfa1a9eca0599e4f059f90424699a904091c7909
dev <koonshen.goh@gmail.com>
2023-11-16 09:39:51 +0800

    Added testcases according to assignment description

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -8,9 +8,43 @@ std::string reverse_words(const std::string &str)
     return "???";
 }
 
-int main()
-{
+void test_empty() {
+    std::string test_str = "";
+    assert(reverse_words(test_str) == "");
+}
+
+void test_simple() {
+    std::string test_str = "reversed";
+    assert(reverse_words(test_str) == "desrever");
+}
+
+void test_multiple() {
+    std::string test_str = "String to be reversed";
+    assert(reverse_words(test_str) == "gnirtS ot eb desrever");
+}
+
+void test_alphanumeric() {
+    std::string test_str = "fe80::1ff:fe23:4567:890a";
+    assert(reverse_words(test_str) == "08ef::ff1:32ef:7654:a098");
+}
+
+void test_complex() {
     std::string test_str = "String; 2be reversed...";
     assert(reverse_words(test_str) == "gnirtS; eb2 desrever...");
+}
+
+void test_all() {
+    test_empty();
+    test_simple();
+    test_multiple();
+    test_alphanumeric();
+    test_complex();
+}
+
+int main()
+{
+    test_all();
+    // std::string test_str = "String; 2be reversed...";
+    // assert(reverse_words(test_str) == "gnirtS; eb2 desrever...");
     return 0;
 }
